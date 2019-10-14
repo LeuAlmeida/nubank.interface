93dbd640102e8d4bad5e9e57288e41693efc5b00
Léu Almeida <leo@webid.net.br>
2019-10-14 18:51:17 -0300

    Update Main index with PanGestureHandler animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,40 +1,74 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
 
-import { 
+import {
   Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation
 } from './styles';
 
 export default function Main() {
+  
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true },
+  )
+
+  function onHandlerStateChange(event) {
+
+  }
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
-        
-        <Card>
-          <CardHeader>
+        <Menu translateY={translateY} />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp',
+              }),
+            }],
+          }}>
+            <CardHeader>
               <Icon name="attach-money" size={28} color="#666" />
               <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ Dinheiros</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 20,00 recebida de Diego Schell Fernandes hoje às 06:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ Dinheiros</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Diego Schell Fernandes hoje às 06:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   )
 };
