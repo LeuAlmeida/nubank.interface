80710c8745e06d45ac3685f2ca9a032d62841668
Léu Almeida <leo@webid.net.br>
2019-10-14 18:50:27 -0300

    Update Tabs Component with translateY animation

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -4,9 +4,22 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, TabsContainer, TabItem, TabText } from './styles';
 
-export default function Tabs() {
+export default function Tabs({ translateY }) {
   return (
-    <Container>
+    <Container style={{
+      transform: [{
+        translateY: translateY.interpolate({
+          inputRange: [0, 380],
+          outputRange: [0, 30],
+          extrapolate: 'clamp',
+        }),
+      }],
+      opacity: translateY.interpolate({
+        inputRange: [0, 380],
+        outputRange: [1, 0.3],
+        extrapolate: 'clamp',
+      }),
+    }}>
       <TabsContainer>
         <TabItem>
           <Icon name="person-add" size={24} color="#FFF" />
