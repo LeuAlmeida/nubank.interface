b94a0ac8f8def2cf578c5ac2cecd5f5856dd3753
Léu Almeida <leo@webid.net.br>
2019-10-14 17:57:29 -0300

    Update container paddings

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   align-items: center;
-  padding: 40px 0 30px;
+  padding: 15px 0 7px;
 `;
 
 export const Top = styled.View`
