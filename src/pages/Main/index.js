a87b5b46293c811b9e13df31b3469eee1112219f
Léu Almeida <leo@webid.net.br>
2019-10-14 15:00:09 -0300

    Update Main content and stylesheets

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+
+import { 
+  Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation
+} from './styles';
 
 export default function Main() {
   return (
-    <Container />
+    <Container>
+      <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ Dinheiros</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Diego Schell Fernandes hoje às 06:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
+    </Container>
   )
 };
 
