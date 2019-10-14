b7e72f0c58b90807b86ffa544a9e630a57637e9c
Léu Almeida <leo@webid.net.br>
2019-10-14 14:19:10 -0300

    Update header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { 
+  Container, Top, Logo, Title
+} from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import logo from '~/assets/Nubank_Logo.png'
 
 export default function Header() {
   return (
-    <Container />
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>Léu Almeida</Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+    </Container>
   );
 }
